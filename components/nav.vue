f18504319bec2e7880c772e5fd2cac1f5492a2c3
emilyypalm <u24758991@tuks.co.za>
2025-04-20 23:12:11 +0200

    Add files via upload

---
+++ components/nav.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+      <ul>
+        <li><NuxtLink to="/">Emily Palm</NuxtLink></li>
+        <li><NuxtLink to="/projects">Projects</NuxtLink></li>
+        <li><NuxtLink to="/contacts">Contacts</NuxtLink></li>
+      </ul>
+    </div>
+  </template>
